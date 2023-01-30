f80835d4baef8a1b3dac66b0d4b71bb3ee9f3531
Francis van Oordt <francis.vanoordtlahoz@mail.mcgill.ca>
2023-01-30 16:22:52 -0500

    cleaning some trial code

--- grizzlies_for_move.R
+++ grizzlies_for_move.R
@@ -54,6 +54,10 @@ myStackDF %>%
   )
 
 
+#make animations, need to be sure individual move in the same timeframe
+
+#should filter for the time frames that have the most 
+#individuals or tracks in consecutive time
 library(gganimate)
 myStackDF <- as.data.frame(movedata)
 ggplot(data = myStackDF, aes(x = x, y = y, color = trackId)) + 
@@ -62,60 +66,6 @@ ggplot(data = myStackDF, aes(x = x, y = y, color = trackId)) +
   theme_bw() + coord_cartesian()+
   transition_reveal(timestamps) +
   labs(title = 'Date: {frame_along}')
-
-myStackDF %>% 
-  group_by(trackId) %>% 
-  summarise(
-    timeRangeb = range(time)[1],
-    timeRangee = range(time)[2]
-  )
-myStackDF <- myStackDF %>% 
-  filter(trackId == c("CF10", "CF2"))
-
-###################
-dataframe1$DATE_ <- lubridate::date(dataframe1$datetime)
-p <- ggplot()+ 
-  geom_path(data = dataframe1, 
-            aes(x = LONGITUDE , y = LATITUDE, color = Bear_ID), 
-            alpha = 0.3)+
-  geom_point(data = dataframe1, 
-             aes(x = LONGITUDE, y = LATITUDE, color = Bear_ID),
-             alpha = 0.7, shape=19, size = 2)+
-  theme_bw()+
-  theme(legend.position = "none")+
-  transition_reveal(DATE_) +
-  labs(title = 'Date: {frame_along}') 
-
-
-p
-###################
-timeLags <- move::timeLag(movedata, units="hours")
-
-distances <- move::distance(movedata)
-
-speeds <- move::speed(movedata)
-
-df1 <- as.data.frame(do.call(cbind, timeLags))
-
-df2 <- pivot_longer(data = df1, cols = colnames(df1), names_to = "Bear_ID", values_to = "timelags1")
-
-hist(df2$timelags1)
-
-##for the other parameter, need to make the new df and pivot it as well
-df3 <- as.data.frame(do.call(cbind, distances))
-
-df4 <- pivot_longer(data = df3, cols = colnames(df3), names_to = "Bear_ID", values_to = "distances")
-
-hist(df4$distances)
-
-
-#for speed
-
-df5 <- as.data.frame(do.call(cbind, speeds))
-
-df6 <- pivot_longer(data = df5, cols = colnames(df5), names_to = "Bear_ID", values_to = "speeds")
-
-hist(df6$speeds)
 
 
 ################# Loop for bursting
