63aa7470a8a4d8c4694ad592be355b27ed3a7ef5
Francis van Oordt <francis.vanoordtlahoz@mail.mcgill.ca>
2023-02-02 21:42:06 -0500

    adding some speed treshold classif

--- grizzlies_for_move.R
+++ grizzlies_for_move.R
@@ -48,12 +48,70 @@ myStackDF %>%
   group_by(trackId) %>% 
   summarise(
     MeanSpeed = mean(speed, na.rm=TRUE),
+    minSpeed = min(speed, na.rm=TRUE),
+    maxSpeed = max(speed, na.rm=TRUE),
     MeanLag = mean(timeLag, na.rm=TRUE),
     MeanDist = mean(distance, na.rm=TRUE),
-    timeRangeb = range(time)[1],
-    timeRangee = range(time)[2]
+    minDis= min(distance, na.rm=TRUE),
+    maxDist = max(distance, na.rm=TRUE),
+    #timeRangeb = range(time)[1],
+    #timeRangee = range(time)[2]
   )
 
+### look at speeds for individual bears
+par(mfrow = c(3,2), 
+    mar = c(2,2,2,2)) 
+
+for (i in unique(myStackDF$trackId)){
+  
+  onebear <- myStackDF %>% 
+    filter(trackId == i)
+  hist(onebear$speed, main = i)
+}
+
+
+#make simple classification by speed threshold
+#PLAY with this values a bit, either decide 3 or 4 classes
+myStackDF <- myStackDF %>% 
+    mutate(
+        colorSpeed = case_when(
+          speed >= 0 & speed < 0.01~ "red",
+        speed > 0.01 & speed < 0.1  ~ "black",
+        speed > 0.1 & speed < 0.3 ~ "blue",
+        speed > 0.3 ~ "green"
+          )
+    
+  )
+#check out which are red
+myStackDF %>% 
+  filter(colorSpeed == "red")
+
+#if using a basemap, crop it first, this Canada one is too big 
+#canmap<-sf::st_read("gadm36_CAN_0.shp")
+
+plot_listFULL <- list()
+
+for (i in unique(myStackDF$trackId)) {
+ onebear <- myStackDF %>% 
+    filter(trackId == i)
+  
+p <- ggplot(data = onebear, aes(x=x , y=y)) +
+  #geom_sf(data = canmap, aes())+ #map takes long to load
+  #coord_sf(crs = 4326, xlim = range(onebear$x) , ylim = range(onebear$y)) + # do not do general limits
+  geom_path(color = onebear$colorSpeed)+ # custom colours need to be listed as vectors, not as part of the df
+  geom_point(color = onebear$colorSpeed)+# custom colours need to be listed as vectors, not as part of the df
+  #guides(alpha = "none", color = "none", size = "none")+
+  #ggtitle(i)+
+  theme_bw()+
+  theme(legend.position = "none")+
+  xlab(NULL)+
+  ylab(NULL)+
+  ggtitle(i)
+  print(p)
+  plot_listFULL[[i]] <- p
+  
+}
+cowplot::plot_grid(plotlist = plot_listFULL)
 
 #make animations, need to be sure individual move in the same timeframe
 
@@ -73,13 +131,9 @@ ggplot(data = myStackDF, aes(x = x, y = y, color = trackId)) +
 library(maptools)
 
 listbursts <- list() #create an empty list to throw in the individual bursts
-
 allburstsdf<-NULL # create an empty dataframe to join all individuals burst now as dataframes
-
 par(mfrow = c(3,2), 
     mar = c(2,2,2,2)) #set the plotting paramters for 4 plots in one panel, only works for base R plots
-
-
 for (i in unique(movedata@trackId)) { #loop over the moveStack object using the trackIDs
   
   oneanimal <- movedata[[i]] #subset 1 animal from the moveStack
