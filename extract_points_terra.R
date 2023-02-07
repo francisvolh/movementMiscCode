8110d7f77fd3f79b5464a154a47626f3d36f9f33
Francis van Oordt <francis.vanoordtlahoz@mail.mcgill.ca>
2023-02-07 11:42:03 -0500

    adding gitignore folder and updates

--- extract_points_terra.R
+++ extract_points_terra.R
@@ -14,6 +14,35 @@ my.sp.point <- terra::vect(gps.points, geom = c("LONGITUDE", "LATITUDE"), crs = 
 w <- terra::project(my.sp.point, r)
 
 #extract the points and add them to the gps.points database
-gps.points$elev <- terra::extract(r, w)
+a <- terra::extract(r, w)
+gps.points$aspect <- a[,2]
+head(gps.points)
+
+
+#clean df a bit to model 
+gps.points<-gps.points %>% 
+ mutate(
+  Used = as.factor(Used)) 
+head(gps.points)
+summary(gps.points$Used)
+class(gps.points$Used)
+
+gps.points<-gps.points%>% 
+   mutate(
+        PA = case_when(Used == "TRUE" ~ 1,
+                       Used == "FALSE" ~ 0
+    )  
+  )
 
 head(gps.points)
+
+
+#try so gLMs
+
+modfull <- lme4::glmer(data=gps.points, Used ~ aspect + elev + for_road+ (1|id), family = binomial)
+mod2 <- lme4::glmer(data=gps.points, Used ~ aspect + (1|id), family = binomial)
+mod1 <- lme4::glmer(data=gps.points, Used ~  elev + (1|id), family = binomial)
+summary(mod1)
+
+mod3 <- lme4::glmer(data=gps.points, Used ~  for_road+ (1|id), family = binomial)
+summary(mod3)
